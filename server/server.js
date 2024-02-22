d0708cecee0819c35a189be8afe24ca5b47117c8
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-22 21:01:03 +0300

    update all

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// server.js
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Define API routes or other routes here as needed
+
+// Catch-all route to serve React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
